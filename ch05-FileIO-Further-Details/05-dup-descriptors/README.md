eddb8eaf915e667ed5dcc784f304c39c8968e197
Sergio Garcia <segarciat314@gmail.com>
2024-03-13 20:52:19 -0400

    Complete exercise 05-05

---
+++ ch05-FileIO-Further-Details/05-dup-descriptors/README.md
@@ -0,0 +1,18 @@
+# Exercise 05-05
+
+Write a program to verify that duplicated file descriptors share a file offset and open file
+status flags.
+
+## Solution
+
+The current implementation accepts a single command-line argument which is meant to be a file.
+It is opened for reading with `open`, and the returned descriptor is used to create a duplicate
+descriptor is created with `dup()`.
+
+Recall that the file offset is set to 0 at the start. My implementation sets the offset to
+end-of-file using `lseek(fd, 0, SEEK_END)` on the initial fd, then compared against the result of
+the current offset on the duplicate fd, this time with a call `lseek(dupfd, 0, SEEK_CUR)`.
+
+For the open file status flags, I was careful to use the flags returned by `fcntl(fd, F_GETFL)`,
+and not the ones passed initially to `open()`, when comparing against the status flag
+returned by `fcntl(dupfd, F_GETFL)`.
---
+++ ch05-FileIO-Further-Details/05-dup-descriptors/verify_dup_descriptors.c
@@ -0,0 +1,36 @@
+#include "tlpi_hdr.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+int
+main(int argc, char* argv[])
+{
+	if (argc != 2 || strcmp(argv[1], "--help") == 0)
+		usageErr("%s file\n", argv[0]);
+
+	int fd = open(argv[1], O_RDONLY | O_SYNC);
+	if (fd == -1)
+		errExit("open");
+	
+	int dupfd = dup(fd);
+	if (dupfd == -1)
+		errExit("dup");
+	
+	int oldFlags, dupFlags;
+	if ((oldFlags = fcntl(fd, F_GETFL)) == -1 || (dupFlags = fcntl(fd, F_GETFL)) == -1)
+		errExit("fcntl GETFL");
+	if (oldFlags != dupFlags)
+		errExit("duplicate file descriptor flags differ");
+	
+	ssize_t eofOffset = lseek(fd, 0, SEEK_END);
+	if (eofOffset == -1)
+		errExit("lseek");
+	
+	ssize_t curDupOffset = lseek(dupfd, 0, SEEK_CUR);
+	if (curDupOffset == -1)
+		errExit("lseek");
+	if (eofOffset != curDupOffset)
+		errExit("Offsets differ");
+	printf("Flags and offsets match!\n"
+			"Flags: %d\nOffsets:%lld\n", oldFlags, (long long) eofOffset);
+}
